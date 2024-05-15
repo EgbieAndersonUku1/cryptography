cbb8204645c654c0a07c0b82bda27f7d63e4ac01
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-05-15 13:57:35 +0100

    The class for the reverseCipher was in the wrong folder

--- reverseCipher/Js/class/reverse_cipher.js
+++ reverseCipher/Js/class/reverse_cipher.js
@@ -1,30 +1,50 @@
 
-
-function encrypt(plainText) {
-   return translate(plainText);
-
-}
-
-function decrypt(cipher) {
-    return translate(cipher);
-}
-
-function translate(text) {
-    let textLength = text.length;
-    let translated = [];
-
-    while (textLength >= 0) {
-        let lastChar = text[textLength];
-        translated.push(lastChar);
-        textLength -= 1;
+class ReverseCipher{
+    
+    constructor (plainText) {
+        this.plainText   = plainText;
+        this._translated = null;
     }
 
-    return translated.join("");
+    encrypt() {
+        let translated = []
+
+        for (let i = this.plainText.length - 1; i >= 0; i--) {
+            translated.push(this.plainText[i]);
+        }
+        this._translated = translated.join("");
+    }
+
+    decrypt() {
+
+        if (!(this._translated)) {
+            return;
+        }
+
+        let translated = []
+
+        for (let i = this._translated.length - 1; i >= 0; i--) {
+            translated.push(this._translated[i]);
+        }
+        this._translated = translated.join("");
+    }
+
+    show() {
+        return this._translated;
+    }
 }
 
-const plainText  = "The way of the dragon";
-const cipher     = encrypt(plainText);
-const normalText = decrypt(cipher);
+const plainText = "This is the way of the dragon";
+const reverseCipher = new ReverseCipher(plainText);
 
+reverseCipher.encrypt();
+const cipher = reverseCipher.show();
 console.log(cipher);
-console.log(normalText);
+
+
+reverseCipher.decrypt();
+const translated = reverseCipher.show();
+
+
+console.log(translated)
+
--- reverseCipher/Js/function/reverse_cipher.js
+++ reverseCipher/Js/function/reverse_cipher.js
@@ -1,50 +1,30 @@
 
-class ReverseCipher{
-    
-    constructor (plainText) {
-        this.plainText   = plainText;
-        this._translated = null;
-    }
 
-    encrypt() {
-        let translated = []
+function encrypt(plainText) {
+   return translate(plainText);
 
-        for (let i = this.plainText.length - 1; i >= 0; i--) {
-            translated.push(this.plainText[i]);
-        }
-        this._translated = translated.join("");
-    }
-
-    decrypt() {
-
-        if (!(this._translated)) {
-            return;
-        }
-
-        let translated = []
-
-        for (let i = this._translated.length - 1; i >= 0; i--) {
-            translated.push(this._translated[i]);
-        }
-        this._translated = translated.join("");
-    }
-
-    show() {
-        return this._translated;
-    }
 }
 
-const plainText = "This is the way of the dragon";
-const reverseCipher = new ReverseCipher(plainText);
+function decrypt(cipher) {
+    return translate(cipher);
+}
 
-reverseCipher.encrypt();
-const cipher = reverseCipher.show();
+function translate(text) {
+    let textLength = text.length;
+    let translated = [];
+
+    while (textLength >= 0) {
+        let lastChar = text[textLength];
+        translated.push(lastChar);
+        textLength -= 1;
+    }
+
+    return translated.join("");
+}
+
+const plainText  = "The way of the dragon";
+const cipher     = encrypt(plainText);
+const normalText = decrypt(cipher);
+
 console.log(cipher);
-
-
-reverseCipher.decrypt();
-const translated = reverseCipher.show();
-
-
-console.log(translated)
-
+console.log(normalText);
